fc70b0c1d7e78f44c3f9ccd51cbecfd2827d883f
anandverma762@gmail.com <anandverma762@gmail.com>
2023-05-11 12:21:58 +0530

    Full updated MVC pattern project

--- MVC/models/productmodels.js
+++ MVC/models/productmodels.js
@@ -1,14 +1,40 @@
 const productarr =[];
+const fs=require('fs');
+const path=require('path')
+const rootdir=require('../util/path');
+const filepath=path.join(rootdir,'data','products.json');
+
+const getproduct=(cb)=>{
+    fs.readFile(filepath,(err,contentfile)=>{
+        if(err){
+            cb([]);
+        }else{
+
+            cb(JSON.parse(contentfile))
+        }
+    });
+}
 module.exports=class Product {
     constructor(t){
             this.title=t;
     }
 
     save(){
-        productarr.push(this);
+
+        getproduct(productarr=>{
+            productarr.push(this);
+            fs.writeFile(filepath,JSON.stringify(productarr),(err)=>{
+                console.log(err);
+            })
+        })
+       
+            
+        
+
     }
 
-    static fetchAll(){
-        return productarr;
+    static fetchAll(cb){
+        getproduct(cb)
+
     }
 }
